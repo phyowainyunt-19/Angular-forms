ad02a2b989cf1aaeca59e1b9ca8e600fe6f0daa4
PhyoWaiNyunt <50352633+pwn-69@users.noreply.github.com>
2022-07-25 11:44:26 +0630

    TDF binding data to a model

--- tdf/src/app/app.component.ts
+++ tdf/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   topics = ['Angular', 'React', 'Vue']
+
+  //useModel
+  userModel = new User('Pwn', 'pwn@test.com', 55559999, '','morning', false);
 }
