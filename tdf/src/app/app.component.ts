ba8c04bb55ab8d094d5a83def8e002c7ea11daa5
PhyoWaiNyunt <50352633+pwn-69@users.noreply.github.com>
2022-08-02 11:22:59 +0630

    TDF submitting form data 1

--- tdf/src/app/app.component.ts
+++ tdf/src/app/app.component.ts
@@ -1,20 +1,40 @@
 import { Component } from '@angular/core';
+import { EnrollmentService } from './enrollment.service';
 import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  topics = ['Angular', 'React', 'Vue']
+  topics = ['Angular', 'React', 'Vue'];
   topicHasError = true;
 
   //useModel
-  userModel = new User('Pwn', 'pwn@test.com', 5555599999, 'default', 'morning', false);
+  userModel = new User(
+    'Pwn',
+    'pwn@test.com',
+    5555599999,
+    'default',
+    'morning',
+    false
+  );
+
+  constructor(private _enrollService: EnrollmentService) { }
 
   //validate topic
   validateTopic(value) {
-    value === 'default' ? this.topicHasError = true : this.topicHasError = false;
+    value === 'default'
+      ? (this.topicHasError = true)
+      : (this.topicHasError = false);
+  }
+
+  //submit form
+  onSubmit() {
+    this._enrollService.enroll(this.userModel).subscribe({
+      complete: () => { console.log(this.userModel) },
+      error: () => { console.error() }
+    });
   }
 }
